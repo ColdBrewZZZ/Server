f76f470e748f04aef02dd4a786c4b341032c3f93
zoe <z.g.bradford@gmail.com>
2023-09-14 13:09:19 -0500

    user_cart insert

--- myapp/routes/user_cart.js
+++ myapp/routes/user_cart.js
@@ -1,11 +1,56 @@
 var express = require('express');
 var router = express.Router();
-
-/* this is user cart */
-router.get('/', function(req, res, next) {
-  res.send('this is the info from user cart table');
+var mysql = require('mysql2');
+require('dotenv').config();
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 
+router.use(express.json());
+
+
+router.get('/', function(req, res, next) {
+  connection.query('SELECT * FROM user_cart', (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+
+router.post('/', function (req, res, next) {
+  
+  const { user_id, item_id, quantity} = req.body;
+
+  
+  const insertQuery = `
+    INSERT INTO user_cart (user_id, item_id, quantity)
+    VALUES (?, ?, ?)
+  `;
+
+  
+  connection.query(
+    insertQuery,
+    [user_id, item_id, quantity],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+
+     
+      res.status(201).json({ message: 'user_cart updated successfully', insertedId: result.insertId });
+    }
+  );
+});
 
 module.exports = router;
